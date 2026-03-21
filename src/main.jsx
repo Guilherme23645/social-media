acebb50f40ed6746909450e4e16cafe01b492a26
Guilherme23645 <gui.depaula236@gmail.com>
2026-03-21 19:22:31 -0300

    fix: add BrowserRouter tag

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './assets/styles/index.css'
 import Pages from './pages'
+import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Pages />
+    <BrowserRouter>
+      <Pages />
+    </BrowserRouter>
   </StrictMode>,
 )
