428446c5737d4532c553db249d0262a603f7ff62
Guilherme23645 <gui.depaula236@gmail.com>
2026-03-27 20:17:10 -0300

    feat: define Home as index and add login route

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,13 +2,15 @@ import { Route, Routes } from "react-router-dom"
 import Home from "./Home"
 import NotFound from "./NotFound"
 import SignUp from "./SignUp"
+import Login from "./Login"
 
 const Pages = () => {
   return (
     <Routes>
-        <Route path="/" element={<Home />}/>
+        <Route index element={<Home />}/>
         <Route path="*" element={<NotFound />}/>
         <Route path="signup" element={<SignUp />}/>
+        <Route path="login" element={<Login />}/>
     </Routes>
   )
 }
